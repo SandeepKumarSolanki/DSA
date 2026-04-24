1be3a166b5b57ab5a1cf1a31337cdc6c630de73e
Sandeep Kumar <166508938+SandeepKumarSolanki@users.noreply.github.com>
2026-04-24 08:43:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
-    int t[46];
-    int solve(int n) {
-        if (n < 0) 
-            return 0;
-        if (t[n] != -1) 
-            return t[n];
-        if (n == 0) 
-            return 1;
-        int step_one = solve(n - 1);
-        int step_two = solve(n - 2);
-
-        return t[n] = step_one + step_two;
-    }
     int climbStairs(int n) {
-        memset(t, -1, sizeof(t));
-        return solve(n);
+        if(n == 1 || n == 2 || n == 3)
+            return ;
+        vector<int> arr(n + 1);
+        arr[1] = 1;
+        arr[2] = 2;
+        arr[3] = 3;
+        for(int i = 3; i <= n; i++) 
+            arr[i] = arr[i - 1] + arr[i - 2];
+        
+        return arr[n];
     }
-
 };
